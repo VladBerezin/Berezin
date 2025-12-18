362c1e401c46bacaa04fecddb01a3f192bfa7551
VladBerezin <berezinvlad565@gmail.com>
2025-12-19 01:17:43 +0300

    Add files via upload

---
+++ Lab_13/ServiceFeatures.java
@@ -0,0 +1,13 @@
+package Lab_13;
+
+public interface ServiceFeatures {
+    void showDetails();
+    default void addReview(String review) {
+        System.out.println("Отзыв добавлен: " + review);
+    }
+
+
+    static void showServiceInfo() {
+        System.out.println("Информация о сервисе доступна");
+    }
+}
